40bfd6828500c16964f0389861eb4818d1931c9f
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2018-09-27 12:13:15 +0200

    Added the `--in-place` option to the package command.
    Since options can't have a default value of `true` (see
    https://robo.li/getting-started/#options), we had to revert the option
    for the command, so that it still uses the temporary directory to create
    the archive.

--- src/Commands/PackageProject.php
+++ src/Commands/PackageProject.php
@@ -7,13 +7,14 @@ trait PackageProject
 
     use \DigipolisGent\Robo\Task\Package\Traits\PackageProjectTrait;
 
-    public function digipolisPackageProject($archiveFile, $dir = null, $opts = ['ignore|i' => ''])
+    public function digipolisPackageProject($archiveFile, $dir = null, $opts = ['ignore|i' => '', 'in-place' => false])
     {
         if (is_callable([$this, 'readProperties'])) {
             $this->readProperties();
         }
         $this->taskPackageProject($archiveFile, $dir)
             ->ignoreFileNames(array_map('trim', explode(',', $opts['ignore'])))
+            ->useTmpDir(!$opts['in-place'])
             ->run();
     }
 }
