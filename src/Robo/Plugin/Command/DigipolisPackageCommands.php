074eadf9be9a18c037e7811ed43c8379b1531db0
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2022-05-03 17:29:11 +0200

    Support Robo 3, PHP8

---
+++ src/Robo/Plugin/Command/DigipolisPackageCommands.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace DigipolisGent\Robo\Task\Package\Robo\Plugin\Commands;
+
+use DigipolisGent\Robo\Task\General\Common\DigipolisPropertiesAwareInterface;
+use Robo\Symfony\ConsoleIO;
+
+class DigipolisPackageCommands extends \Robo\Tasks implements DigipolisPropertiesAwareInterface, \Robo\Contract\ConfigAwareInterface
+{
+    use \DigipolisGent\Robo\Task\Package\Tasks;
+    use \DigipolisGent\Robo\Task\General\Common\DigipolisPropertiesAware;
+    use \Consolidation\Config\ConfigAwareTrait;
+
+    /**
+     * @command digipolis:theme-compile
+     */
+    public function digipolisThemeCompile(ConsoleIO $io, $dir = null, $buildCommand = 'compile')
+    {
+        $this->readProperties();
+        return $this->taskThemeCompile($dir, $buildCommand);
+    }
+
+    /**
+     * @command digipolis:theme-clean
+     */
+    public function digipolisThemeClean(ConsoleIO $io, $dir = null)
+    {
+        $this->readProperties();
+        return $this->taskThemeClean($dir)->run();
+    }
+
+    /**
+     * @command digipolis:package-project
+     */
+    public function digipolisPackageProject(ConsoleIO $io, $archiveFile, $dir = null, $opts = ['ignore|i' => ''])
+    {
+        $this->readProperties();
+        return $this->taskPackageProject($archiveFile, $dir)
+            ->ignoreFileNames(array_map('trim', explode(',', $opts['ignore'])))
+            ->run();
+    }
+}
